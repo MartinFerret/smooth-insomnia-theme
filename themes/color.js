8e8b5ea581a772fb93b0b7a17480c1da765c1ed3
Martin Ferret <firdaouszm@gmail.com>
2024-05-10 08:57:14 +0200

    update colors

--- themes/color.js
+++ themes/color.js
@@ -1,7 +1,7 @@
 const COLOR = {
     PURPLE: "#8A2BE2",
-    WHITE: "#FAFAFA",
-    BLACK: "#515E64",
+    WHITE: "#e0e4e8",
+    BLACK: "#1e2428",
     ORANGE: "#FFA500",
     RED: "#FF0000",
     PINK_LIGHT: "#FFC0CB",
